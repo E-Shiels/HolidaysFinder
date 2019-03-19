fa3e64e98f99de7eda0967989f91969967f89364
Eric Shiels <ericshiels2@gmail.com>
2019-03-19 19:12:19 -0400

    Add basic App.js - static

--- src/App.js
+++ src/App.js
@@ -6,8 +6,35 @@ class App extends Component {
     return (
       <div>
       <h2>Find holidays in </h2>
-
+      <select>
+      <option>All</option>
+      <option>Ontario</option>
+      <option>Quebec</option>
+      <option>Manitoba</option>
+      <option>Prince Edward Island</option>
+      </select>
+      <h3>On</h3>
+      <input type="date"/>
+      <br></br>
+      <input type="submit"/>
+      <br></br>
+      <br></br>
+      <br></br>
+      <div>
+      <h5>Holiday Date - Holiday Name</h5>
+      <p>Description</p>
+      <p>Observance</p>
+      <p>Locations</p>
       </div>
+      <br></br>
+      <div>
+      <h5>Holiday Date - Holiday Name</h5>
+      <p>Description</p>
+      <p>Observance</p>
+      <p>Locations</p>
+      </div>
+      </div>
+
     );
   }
 }
