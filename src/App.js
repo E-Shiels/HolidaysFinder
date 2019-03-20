8eb377e441872a63a5df1b390f2c61308643c8ad
Eric Shiels <ericshiels2@gmail.com>
2019-03-20 14:18:36 -0400

    Add more data for testing

--- src/App.js
+++ src/App.js
@@ -80,9 +80,54 @@ class App extends Component {
                   "iso": "us-fl"
               }
           ]
+      },
+      {
+          "name": "Lincoln's Birthday",
+          "description": "Lincoln’s Birthday is observed in the United States in honor of Abraham Lincoln, who was the nation’s 16th president and was known as the Great Emancipator.",
+          "date": {
+              "iso": "2019-02-12",
+              "datetime": {
+                  "year": 2019,
+                  "month": 2,
+                  "day": 12
+              }
+          },
+          "type": [
+              "Local holiday"
+          ],
+          "locations": "CT, IL, NY, MO",
+          "states": [
+              {
+                  "id": 9,
+                  "abbrev": "CT",
+                  "name": "Connecticut",
+                  "exception": null,
+                  "iso": "us-ct"
+              },
+              {
+                  "id": 16,
+                  "abbrev": "IL",
+                  "name": "Illinois",
+                  "exception": null,
+                  "iso": "us-il"
+              },
+              {
+                  "id": 36,
+                  "abbrev": "NY",
+                  "name": "New York",
+                  "exception": null,
+                  "iso": "us-ny"
+              },
+              {
+                  "id": 29,
+                  "abbrev": "MO",
+                  "name": "Missouri",
+                  "exception": null,
+                  "iso": "us-mo"
+              }
+          ]
       }]}/>
       </div>
-
     );
   }
 }
