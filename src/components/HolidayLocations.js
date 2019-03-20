36a8c56281c36cf4907dd59b34c0515ad7ac0238
Eric Shiels <ericshiels2@gmail.com>
2019-03-20 17:43:38 -0400

    Add uuid keys

--- src/components/HolidayLocations.js
+++ src/components/HolidayLocations.js
@@ -1,15 +1,15 @@
 import React from 'react'
 import HolidayLocation from './HolidayLocation.js'
-
+const uuidv4 = require('uuid/v4')
 const HolidayLocations = props => {
   function createHolidayLocations() {
     if (typeof props.locations == "undefined") {
-      return "WHAT"
+      return "WAT"
     } else if (typeof props.locations === "string" || props.locations.length === 1) {
-    return <HolidayLocation location={{name: props.locations}} />
+    return <HolidayLocation key location={{name: props.locations}} />
   } else {
       return props.locations.map((location) => {
-      return <HolidayLocation location={{name: location.name}}/>
+      return <HolidayLocation key={uuidv4()} location={{name: location.name}}/>
     })
   }}
   return (
