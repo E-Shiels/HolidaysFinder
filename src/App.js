50c9f931b93ab1d0d7c4d8b5e6279fd5f8cbd173
Eric Shiels <ericshiels2@gmail.com>
2019-03-22 15:07:00 -0400

    Adds site footer

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 import Holidays from './components/Holidays.js'
 import LocationAndDateInput from './components/LocationAndDateInput.js'
+import SiteFooter from './components/SiteFooter.js'
 let canadaHolidays = require('./data.json');
 
 class App extends Component {
@@ -12,6 +13,7 @@ class App extends Component {
       <br></br>
       <br></br>
       <Holidays holidays={canadaHolidays.holidays}/>
+      <SiteFooter />
       </div>
     );
   }
---
+++ src/components/SiteFooter.js
@@ -0,0 +1,31 @@
+import React from "react";
+
+var style = {
+    backgroundColor: "#F8F8F8",
+    borderTop: "1px solid #E7E7E7",
+    textAlign: "center",
+    padding: "0px 10px 15px 10px",
+    position: "fixed",
+    left: "0",
+    bottom: "0",
+    height: "30px",
+    width: "100%",
+}
+
+var phantom = {
+  display: 'block',
+  padding: '10px',
+  height: '30px',
+  width: '100%',
+}
+
+export default function SiteFooter({ children }) {
+    return (
+        <div>
+            <div style={phantom} />
+            <div style={style}>
+                <p>Website Design and Development by <a href="https://www.resays.com/">Eric Shiels.</a> Holiday data courtesy of <a href="https://calendarific.com/">Calendarific</a></p>
+            </div>
+        </div>
+    )
+}
