c763c4d55a579d470b81b71a775f677a1aa9baf3
Eric Shiels <ericshiels2@gmail.com>
2019-03-26 16:44:15 -0400

    Update HolidaysContainer.js

--- src/components/HolidaysContainer.js
+++ src/components/HolidaysContainer.js
@@ -4,16 +4,31 @@ import HolidaysFilters from './HolidaysFilters'
 
 export default class HolidaysContainer extends React.Component {
   state = {
-    holidaysData: ""
+    holidaysData: []
+  }
+
+  toHolidayObjectsFromJSON(data) {
+    let holidaysArray = [];
+    data.forEach(holiday => {
+      holidaysArray.push({
+        name: holiday.name,
+        description: holiday.description,
+        date: holiday.date,
+        type: holiday.holiday_type,
+        locations: holiday.locations,
+        states: holiday.states
+      })
+    })
+    return holidaysArray
   }
 
   componentDidMount() {
     fetch('http://localhost:3000/api/v1/holidays')
     .then(response => response.json())
     .then(data => {
-      debugger
+      console.log(data)
       this.setState({
-        holidaysData: data
+        holidaysData: this.toHolidayObjectsFromJSON(data)
       })
     })
   }
