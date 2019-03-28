4f06fa3f3972b67892ab51663b8bc7fd42368ee3
Eric Shiels <ericshiels2@gmail.com>
2019-03-28 14:48:50 -0400

    Prettify

--- src/components/HolidaysFilters.js
+++ src/components/HolidaysFilters.js
@@ -1,40 +1,40 @@
-import React from 'react'
-import Picky from 'react-picky'
-import 'react-picky/dist/picky.css'
-import equal from 'fast-deep-equal'
+import React from "react";
+import Picky from "react-picky";
+import "react-picky/dist/picky.css";
+import equal from "fast-deep-equal";
 
 export default class HolidaysFilters extends React.Component {
-
   state = {
     observances: []
-  }
+  };
 
   getListOfObservanceTypes = data => {
     let observanceList = [];
     if (this.props.holidays !== []) {
       this.props.holidays.forEach(holiday => {
         if (holiday.type.includes(",")) {
-          holiday.type.split(',').forEach(type => {
+          holiday.type.split(",").forEach(type => {
             let trimmedType = type.trim();
             if (!observanceList.includes(trimmedType)) {
               observanceList.push(trimmedType);
             }
-          })
+          });
         } else {
           if (!observanceList.includes(holiday.type)) {
             observanceList.push(holiday.type);
           }
         }
-      })
+      });
     }
     this.setState({
       observances: observanceList
-    })
-  }
+    });
+  };
+
 
   componentDidUpdate(prevProps) {
     if (!equal(this.props.holidays, prevProps.holidays)) {
-      this.getListOfObservanceTypes()
+      this.getListOfObservanceTypes();
     }
   }
 
