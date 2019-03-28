f621d0a66c5072baa2bf153c92ba304de51ecf84
Eric Shiels <ericshiels2@gmail.com>
2019-03-28 14:34:18 -0400

    Prettify

--- src/components/EmailConfirm.js
+++ src/components/EmailConfirm.js
@@ -1,22 +1,29 @@
-import React from 'react'
+import React from "react";
 
 export default class EmailConfirm extends React.Component {
+
   saveAndConfirm = event => {
-    event.preventDefault()
-    this.props.nextStep()
-  }
+    event.preventDefault();
+    this.props.nextStep();
+  };
+
   back = event => {
-    event.preventDefault()
-    this.props.prevStep()
-  }
+    event.preventDefault();
+    this.props.prevStep();
+  };
+
   render() {
     const { values } = this.props;
-    return(
+    return (
       <>
-      <p>{values.email}</p>
-        <button type="button" onClick={this.back}>Back</button>
-        <button type="button" onClick={this.saveAndConfirm}>Confirm Email</button>
+        <p>{values.email}</p>
+        <button type="button" onClick={this.back}>
+          Back
+        </button>
+        <button type="button" onClick={this.saveAndConfirm}>
+          Confirm Email
+        </button>
       </>
-    )
+    );
   }
 }
