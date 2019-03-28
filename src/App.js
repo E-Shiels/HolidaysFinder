761f451b247f4417ccca87175e5efe6a6e033bdd
Eric Shiels <ericshiels2@gmail.com>
2019-03-28 15:11:39 -0400

    Prettify

--- src/App.js
+++ src/App.js
@@ -1,18 +1,17 @@
-import React, { Component } from 'react';
-import './App.css';
-import HolidaysContainer from './components/HolidaysContainer.js'
-import SiteFooter from './components/SiteFooter.js'
-import EmailContainer from './components/EmailContainer.js'
-// let canadaHolidays = require('./data.json');
+import React, { Component } from "react";
+import "./App.css";
+import HolidaysContainer from "./components/HolidaysContainer.js";
+import SiteFooter from "./components/SiteFooter.js";
+import EmailContainer from "./components/EmailContainer.js";
 
 class App extends Component {
   render() {
-        let style = {width: '90%'}
+    let style = { width: "90%" };
     return (
       <div style={style}>
-      <HolidaysContainer />
-      <EmailContainer />
-      <SiteFooter />
+        <HolidaysContainer />
+        <EmailContainer />
+        <SiteFooter />
       </div>
     );
   }
