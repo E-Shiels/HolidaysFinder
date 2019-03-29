59b7dc51292928a60a1e950bea8a112551d82ef9
Eric Shiels <ericshiels2@gmail.com>
2019-03-29 15:50:19 -0400

    Working on routing

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import "./App.css";
 import HolidaysContainer from "./components/HolidaysContainer.js";
 import SiteFooter from "./components/SiteFooter.js";
 import EmailContainer from "./components/EmailContainer.js";
+
+import NotFound from './components/NotFound.js'
+
 import {
   BrowserRouter as Router,
   Route,
@@ -14,11 +17,17 @@ class App extends Component {
     let style = { width: "90%" };
     return (
       <Router>
+      <h1>Holidays</h1>
+      <Switch>
+      <Route path="/about" />
+      <Route exact path="/" />
       <div style={style}>
         <HolidaysContainer />
         <EmailContainer />
         <SiteFooter />
       </div>
+      <Route component={NotFound}/>
+      </Switch>
       </Router>
     );
   }
