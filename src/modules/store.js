a7adfe2f0b19c4db584c104cdd6b7fb6cb3f7c53
Eric Shiels <ericshiels2@gmail.com>
2019-03-29 15:55:48 -0400

    Rearranges reduces and adds comments for what to do in the future

--- src/modules/store.js
+++ src/modules/store.js
@@ -1,8 +1,12 @@
 import { createStore, applyMiddleware, combineReducers } from "redux";
 import logger from "redux-logger";
 import thunk from "redux-thunk";
-import reducer from "./modules/reducers/reducer.js";
 import { createStore } from "redux";
+
+import reducer from "./modules/reducers/reducer.js";
+//import other reducers
+
+//combine reducers
 
 const middleware = [thunk];
 
