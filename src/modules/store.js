fdba79eb99e6519faba06813c5c1c87480f73193
Eric Shiels <ericshiels2@gmail.com>
2019-03-29 15:50:26 -0400

    Update store.js

--- src/modules/store.js
+++ src/modules/store.js
@@ -1,3 +1,7 @@
 import { createStore, applyMiddleware, combineReducers } from 'redux';
 import logger from 'redux-logger';
 import thunk from 'redux-thunk';
+import reducer from './modules/reducers/reducer.js'
+import { createStore } from 'redux'
+
+const store = createStore(reducer)
