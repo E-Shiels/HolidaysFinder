2f1a101385af65e595fe045b6df5b72e86eeec67
Eric Shiels <ericshiels2@gmail.com>
2019-03-29 16:14:23 -0400

    Wrapping notfound in a div

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -1,5 +1,5 @@
 import React from 'react'
 
-const notFound = () => <h1>The page you were looking for could not be found.</h1>
+const notFound = () => <div><h1>The page you were looking for could not be found.</h1></div>
 
 export default notFound
