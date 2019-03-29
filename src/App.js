5cfeb8d8f9305de704175951bfcb0ae990137965
Eric Shiels <ericshiels2@gmail.com>
2019-03-29 14:39:10 -0400

    Add routing to App.js

--- src/App.js
+++ src/App.js
@@ -3,16 +3,23 @@ import "./App.css";
 import HolidaysContainer from "./components/HolidaysContainer.js";
 import SiteFooter from "./components/SiteFooter.js";
 import EmailContainer from "./components/EmailContainer.js";
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch
+} from 'react-router-dom'
 
 class App extends Component {
   render() {
     let style = { width: "90%" };
     return (
+      <Router>
       <div style={style}>
         <HolidaysContainer />
         <EmailContainer />
         <SiteFooter />
       </div>
+      </Router>
     );
   }
 }
