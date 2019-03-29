fe94f3563f2d0f379316d35adf2a4e8861018d1b
Eric Shiels <ericshiels2@gmail.com>
2019-03-29 15:49:32 -0400

    Moves store to store.js file

--- src/index.js
+++ src/index.js
@@ -1,15 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import store from './modules/store'
 
-import reducer from './modules/reducers/reducer.js'
+import './index.css';
 
 import { Provider } from 'react-redux'
-import { createStore } from 'redux'
-
-const store = createStore(reducer)
 
 //Allows for hot (re)loading
 if (module.hot) {
@@ -22,7 +19,3 @@ ReactDOM.render(
   </Provider>,
   document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
