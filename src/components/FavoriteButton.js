0adb97f532a3507e515ab21d8ef4495df6a07016
Eric Shiels <ericshiels2@gmail.com>
2019-03-29 21:39:14 -0400

    Add basic parts to favorite button

---
+++ src/components/FavoriteButton.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import SvgIcon from '@material-ui/core/SvgIcon';
+import yellow from '@material-ui/core/colors/yellow';
+import { withStyles } from '@material-ui/core/styles';
+
+
+export default class FavoriteButton extends React.Component {
+  state = {
+
+  }
+
+  render() {
+    return(
+      ""
+    )
+  }
+}
