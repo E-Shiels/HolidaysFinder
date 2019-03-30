d003e2e90652c6745014ac9f38ccc4bc29897323
Eric Shiels <ericshiels2@gmail.com>
2019-03-29 21:40:08 -0400

    Move footer outisde switch statement,  move div to outside switch, prettify

--- src/App.js
+++ src/App.js
@@ -4,30 +4,28 @@ import HolidaysContainer from "./components/HolidaysContainer.js";
 import SiteFooter from "./components/SiteFooter.js";
 import EmailContainer from "./components/EmailContainer.js";
 
-import NotFound from './components/NotFound.js'
+import NotFound from "./components/NotFound.js";
 
-import {
-  BrowserRouter as Router,
-  Route,
-  Switch
-} from 'react-router-dom'
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 class App extends Component {
   render() {
     let style = { width: "90%" };
     return (
       <Router>
-      <h1>Holidays</h1>
-      <Switch>
-      <Route path="/about" />
-      <Route exact path="/" />
-      <div style={style}>
-        <HolidaysContainer />
-        <EmailContainer />
-        <SiteFooter />
-      </div>
-      <Route component={NotFound}/>
-      </Switch>
+        <div style={style}>
+          <h1>Holidays</h1>
+          <Switch>
+            <Route path="/about" />
+            <Route exact path="/" />
+
+            <HolidaysContainer />
+            <EmailContainer />
+
+            <Route component={NotFound} />
+          </Switch>
+          <SiteFooter />
+        </div>
       </Router>
     );
   }
