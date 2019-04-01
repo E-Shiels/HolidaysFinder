244fb1fb248371c22f53ce2d9edf147debaa7994
Eric Shiels <ericshiels2@gmail.com>
2019-04-01 14:19:09 -0400

    Adds checkbox to favorite a holiday

--- src/App.js
+++ src/App.js
@@ -3,20 +3,23 @@ import "./App.css";
 import HolidaysContainer from "./components/HolidaysContainer.js";
 import SiteFooter from "./components/SiteFooter.js";
 import EmailContainer from "./components/EmailContainer.js";
-import FavoriteButton from './components/FavoriteButton.js'; //temp
+import FavoriteButton from './components/FavoriteButton.js';
 
 import NotFound from "./components/NotFound.js";
 
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 class App extends Component {
+  changeDatabase = () => {
+    console.log("CHANGED")
+  }
   render() {
     let style = { width: "90%" };
     return (
       <Router>
         <div style={style}>
           <h1>Holidays</h1>
-          <FavoriteButton /> //temp
+          <FavoriteButton favorite={false} changeDatabase={this.changeDatabase}/>
           <Switch>
             <Route path="/about" />
             <Route exact path="/" />
--- src/components/FavoriteButton.js
+++ src/components/FavoriteButton.js
@@ -1,19 +1,25 @@
 import React from 'react';
-import SvgIcon from '@material-ui/core/SvgIcon';
-import yellow from '@material-ui/core/colors/yellow';
-import { withStyles } from '@material-ui/core/styles';
+// import SvgIcon from '@material-ui/icons/';
+// import yellow from '@material-ui/core/colors/yellow';
+// import { withStyles } from '@material-ui/core/styles';
 
 
 export default class FavoriteButton extends React.Component {
   state = {
+    favorite: this.props.favorite
+  }
 
+  handleChange = () => {
+    this.setState({favorite: !this.state.favorite})
+    this.props.changeDatabase()
   }
 
   render() {
     return(
-      <i class="material-icons">
-        star
-      </i>
+      <>
+        <label htmlFor="favorite">Favorite</label>
+        <input type="checkbox" id="favorite" name="favorite" onChange={this.handleChange} value={this.props.favorite} defaultChecked={this.props.favorite}/>
+      </>
     )
   }
 }
