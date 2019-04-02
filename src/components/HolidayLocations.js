da6a79ce351f832f9bbc100dc2a8aa71e4e506ba
Eric Shiels <ericshiels2@gmail.com>
2019-04-02 13:49:03 -0400

    Fix key in first HolidayLocation instance + Prettify

--- src/components/HolidayLocations.js
+++ src/components/HolidayLocations.js
@@ -1,21 +1,27 @@
-import React from 'react'
-import HolidayLocation from './HolidayLocation.js'
-const uuidv4 = require('uuid/v4')
+import React from "react";
+import HolidayLocation from "./HolidayLocation.js";
+const uuidv4 = require("uuid/v4");
 
 const HolidayLocations = props => {
   function createHolidayLocations() {
     if (typeof props.locations == "undefined") {
-      return "WAT"
-    } else if (typeof props.locations === "string" || props.locations.length === 1) {
-    return <HolidayLocation key location={{name: props.locations}} />
-  } else {
-      return props.locations.map((location) => {
-      return <HolidayLocation key={uuidv4()} location={{name: location.name}}/>
-    })
-  }}
-  return (
-    createHolidayLocations()
-  )
-}
+      return "WAT";
+    } else if (
+      typeof props.locations === "string" ||
+      props.locations.length === 1
+    ) {
+      return (
+        <HolidayLocation key={uuidv4()} location={{ name: props.locations }} />
+      );
+    } else {
+      return props.locations.map(location => {
+        return (
+          <HolidayLocation key={uuidv4()} location={{ name: location.name }} />
+        );
+      });
+    }
+  }
+  return createHolidayLocations();
+};
 
 export default HolidayLocations;
