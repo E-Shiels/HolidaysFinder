5ea64260806dcc3ff30afb50a62c1ac6cb879096
Eric Shiels <ericshiels2@gmail.com>
2019-04-02 13:32:25 -0400

    Prettify

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -1,5 +1,9 @@
-import React from 'react'
+import React from "react";
 
-const notFound = () => <div><h1>The page you were looking for could not be found.</h1></div>
+const notFound = () => (
+  <div>
+    <h1>The page you were looking for could not be found.</h1>
+  </div>
+);
 
-export default notFound
+export default notFound;
