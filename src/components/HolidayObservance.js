adc707764a3c1955f34c7487a79abea34b52222d
Eric Shiels <ericshiels2@gmail.com>
2019-04-02 14:55:28 -0400

    Remove old debugger

--- src/components/HolidayObservance.js
+++ src/components/HolidayObservance.js
@@ -2,7 +2,6 @@ import React from "react";
 
 const HolidayObservance = props => {
   if (props.observance.length > 1) {
-    debugger;
     return <p>{props.observance.join(" - ")}</p>;
   } else {
     return <p>{props.observance}</p>;
