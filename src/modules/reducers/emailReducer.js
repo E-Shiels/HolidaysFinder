5ba22a637c0c6fb1517daf90ece301be6dc36981
Eric Shiels <ericshiels2@gmail.com>
2019-04-03 14:37:38 -0400

    Update emailReducer to move through steps correctly

--- src/modules/reducers/emailReducer.js
+++ src/modules/reducers/emailReducer.js
@@ -3,17 +3,15 @@ const initialState = {
   email: "",
 };
 
-function  email(state = initialState, action) {
+export default function email(state = initialState, action) {
   switch (action.type) {
     case "EMAIL_NEXT_STEP":
-      return action.step + 1;
+      return {...state, step: action.step + 1};
     case "EMAIL_PREV_STEP":
-      return action.step - 1;
+      return {...state, step: action.step - 1};
     case "EMAIL_CHANGE":
       return action.email;
     default:
       return state;
   }
 };
-
-export default email;
