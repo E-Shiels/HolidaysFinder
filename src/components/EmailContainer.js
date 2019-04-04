ae72c4ce3aebeb20111021d8ed430f5d32aa370b
Eric Shiels <ericshiels2@gmail.com>
2019-04-04 11:12:12 -0400

    Remove commented out old code

--- src/components/EmailContainer.js
+++ src/components/EmailContainer.js
@@ -3,22 +3,8 @@ import { connect } from 'react-redux';
 import { ConnectedEmailInput } from "./EmailInput.js";
 import { ConnectedEmailSuccess } from "./EmailSuccess.js";
 import { ConnectedEmailConfirm } from "./EmailConfirm.js";
-//import { emailNextStep, emailPrevStep, emailChange } from '../modules/actions/emailActions.js'
 
 export class EmailContainer extends React.Component {
-  // nextStep = (email) => {
-  //   this.props.dispatch(emailNextStep(this.props.step))
-  //   this.props.dispatch(emailChange(email))
-  // };
-  //
-  // prevStep = () => {
-  //   this.props.dispatch(emailPrevStep(this.props.step))
-  // };
-  //
-  // handleChange = input => event => {
-  //    this.setState({ [input]: event.target.value });
-  //  };
-
   render() {
     const { step } = this.props;
     switch (step) {
