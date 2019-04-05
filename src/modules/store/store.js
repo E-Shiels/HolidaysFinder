1f9770f80eec66814c88407147f7283724684547
Eric Shiels <ericshiels2@gmail.com>
2019-04-05 14:05:42 -0400

    Add holiday reducer to store

--- src/modules/store/store.js
+++ src/modules/store/store.js
@@ -3,10 +3,12 @@ import logger from "redux-logger";
 import thunk from "redux-thunk";
 
 import emailReducer from "../reducers/emailReducer.js";
+import holidayReducer from "../reducers/holidayReducer.js";
 //import other reducers
 
 const rootReducer = combineReducers({
   email: emailReducer,
+  holidays: holidayReducer
   //import other reducers
 })
 
