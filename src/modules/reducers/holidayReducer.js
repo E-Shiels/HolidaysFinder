88360bec094491566fbd6e812538fa537eb38c83
Eric Shiels <ericshiels2@gmail.com>
2019-04-05 14:08:17 -0400

    Add date to initial state and add cases fkr setting location and applying input filters

--- src/modules/reducers/holidayReducer.js
+++ src/modules/reducers/holidayReducer.js
@@ -6,6 +6,7 @@ const initialState = {
   observances: [],
   loading: false,
   error: null,
+  date: '',
 };
 
 export default function holidayReducer(state = initialState, action) {
@@ -13,9 +14,13 @@ export default function holidayReducer(state = initialState, action) {
     case "FETCH_HOLIDAYS_BEGIN":
       return {...state, loading: true, error: null}
     case "FETCH_HOLIDAYS_SUCCESS":
-      return {...state, loading: false, holiday: action.holidays}
+      return {...state, loading: false, holidays: action.holidays}
     case "FETCH_HOLIDAYS_FAILURE":
       return {...state, loading: false, error: action.error, holidays: []}
+    case "APPLY_DATE_AND_LOCATION_FILTER":
+      return {...state, filteredHolidays: action.filteredHolidays}
+    case "SET_LOCATION":
+    return {...state, selectedLocation: action.location}
     default:
       return state;
   }
