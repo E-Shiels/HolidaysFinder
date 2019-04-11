47cd283fef593366ef8413b1e3d80f35139adde5
Eric Shiels <ericshiels2@gmail.com>
2019-04-11 17:31:07 -0400

    Prettify

--- src/components/FavoriteButton.js
+++ src/components/FavoriteButton.js
@@ -1,24 +1,29 @@
-import React from 'react';
-// import SvgIcon from '@material-ui/icons/';
-// import yellow from '@material-ui/core/colors/yellow';
-// import { withStyles } from '@material-ui/core/styles';
+import React from "react";
+
 export default class FavoriteButton extends React.Component {
   state = {
     favorite: this.props.favorite
-  }
+  };
 
   handleChange = () => {
-    this.setState({favorite: !this.state.favorite}, function() {
-    this.props.updateHolidayFavoriteState(this.state.favorite, this.props.id)
-  })
-  }
+    this.setState({ favorite: !this.state.favorite }, function() {
+      this.props.updateHolidayFavoriteState(this.state.favorite, this.props.id);
+    });
+  };
 
   render() {
-    return(
+    return (
       <>
         <label htmlFor="favorite">Favorite</label>
-        <input type="checkbox" id="favorite" name="favorite" onChange={this.handleChange} value={this.props.favorite} defaultChecked={this.props.favorite}/>
+        <input
+          type="checkbox"
+          id="favorite"
+          name="favorite"
+          onChange={this.handleChange}
+          value={this.props.favorite}
+          defaultChecked={this.props.favorite}
+        />
       </>
-    )
+    );
   }
 }
