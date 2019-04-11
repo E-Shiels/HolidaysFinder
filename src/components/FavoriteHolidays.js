1c621a13db9d2e0b47a5468cdbfee525b353ba63
Eric Shiels <ericshiels2@gmail.com>
2019-04-11 16:58:41 -0400

    Move render method into render() statement
    To be refactored later

--- src/components/FavoriteHolidays.js
+++ src/components/FavoriteHolidays.js
@@ -3,56 +3,13 @@ import { connect } from "react-redux";
 import { ConnectedHoliday } from "./Holiday.js";
 import {
   fetchHolidaysBegin,
-  fetchHolidaysSuccess,
+  fetchFavoriteHolidaysSuccess,
   fetchHolidaysFailure
 } from "../modules/actions/holidayActions.js";
 
 export default class FavoriteHolidays extends React.Component {
   state = {
     favoriteHolidays: []
-  };
-
-  renderFavoriteHolidays = (favoriteHolidays) => {
-    if (favoriteHolidays.length === 0) {
-      return "You have no favorites."
-  } else {
-  favoriteHolidays.map(holiday => {
-    let locationsList = [""];
-    if (holiday.states.length === 1) {
-      locationsList.push({
-        name: holiday.states[0].name
-      });
-    } else if (holiday.states === "All") {
-      locationsList.push({
-        name: "Canada (All)"
-      });
-    } else {
-      holiday.states.split(",").forEach(state => {
-        locationsList.push({
-          name: state.trim()
-        });
-      });
-    }
-    let observanceList = [];
-    if (holiday.type.includes(",")) {
-      holiday.type.split(", ").forEach(type => {
-        observanceList.push(type.trim());
-      });
-    } else {
-      observanceList.push(holiday.type);
-    }
-
-    return (<ConnectedHoliday
-        key={holiday.id}
-        id={holiday.id}
-        date={holiday.date}
-        name={holiday.name}
-        observance={observanceList}
-        description={holiday.description}
-        locations={locationsList}
-        favorite={holiday.favorite}/>);
-  });
-  }
   };
 
   toHolidayObjectsFromJSON = data => {
@@ -80,27 +37,68 @@ export default class FavoriteHolidays extends React.Component {
       .then(response => response.json())
       .then(json => {
         this.props.dispatch(
-          fetchHolidaysSuccess(this.toHolidayObjectsFromJSON(json))
+          fetchFavoriteHolidaysSuccess(this.toHolidayObjectsFromJSON(json))
         );
       })
-      .then()
       .catch(error => this.props.dispatch(fetchHolidaysFailure(error)));
   }
 
   render() {
-    //debugger
-     if (!this.props.loading){
-     return this.renderFavoriteHolidays(this.props.holidays);
-   } else {
-    return null
+    if (this.props.loading) {
+      return "LOADING"
+    } else {
+    if (!this.props.favoriteHolidays.length) {
+      return "You have no favorites."
+    } else {
+     return this.props.favoriteHolidays.map(holiday => {
+       let locationsList = [""];
+       if (holiday.states.length === 1) {
+         locationsList.push({
+           name: holiday.states[0].name
+         });
+       } else if (holiday.states === "All") {
+         locationsList.push({
+           name: "Canada (All)"
+         });
+       } else {
+         holiday.states.split(",").forEach(state => {
+           locationsList.push({
+             name: state.trim()
+           });
+         });
+       }
+       let observanceList = [];
+       if (holiday.type.includes(",")) {
+         holiday.type.split(", ").forEach(type => {
+           observanceList.push(type.trim());
+         });
+       } else {
+         observanceList.push(holiday.type);
+       }
+
+       return (
+         <ConnectedHoliday
+           key={holiday.id}
+           id={holiday.id}
+           date={holiday.date}
+           name={holiday.name}
+           observance={observanceList}
+           description={holiday.description}
+           locations={locationsList}
+           favorite={holiday.favorite}
+         />
+       );
+     });
    }
    }
+ }
 }
 
 const mapStateToProps = state => {
   return {
     holidays: state.holidays.holidays,
     loading: state.holidays.loading,
+    favoriteHolidays: state.holidays.favoriteHolidays
     //favoriteHolidays: state.holidays.favoriteHolidays
   };
 };
