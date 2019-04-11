bc4800dddb67afd532c01e8949e813cba2d2f5d3
Eric Shiels <ericshiels2@gmail.com>
2019-04-11 17:10:08 -0400

    Add header to 404 error page

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -2,7 +2,8 @@ import React from "react";
 
 const notFound = () => (
   <div>
-    <h1>The page you were looking for could not be found.</h1>
+    <h1>Error - 404</h1>
+    <h3>The page you were looking for could not be found.</h3>
   </div>
 );
 
