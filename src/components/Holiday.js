576d7acc8a17de81feaf7ee736f6926ce84c6c0f
Eric Shiels <ericshiels2@gmail.com>
2019-04-11 17:39:09 -0400

    Prettify

--- src/components/Holiday.js
+++ src/components/Holiday.js
@@ -1,51 +1,53 @@
-import React from 'react'
-import HolidayDescription from './HolidayDescription'
-import HolidayHeader from './HolidayHeader'
-import HolidayLocations from './HolidayLocations'
-import HolidayObservance from './HolidayObservance'
-import FavoriteButton from './FavoriteButton.js';
+import React from "react";
+import HolidayDescription from "./HolidayDescription";
+import HolidayHeader from "./HolidayHeader";
+import HolidayLocations from "./HolidayLocations";
+import HolidayObservance from "./HolidayObservance";
+import FavoriteButton from "./FavoriteButton.js";
 
 import { connect } from "react-redux";
-import {
-  changeFavorite
-} from "../modules/actions/holidayActions.js";
+import { changeFavorite } from "../modules/actions/holidayActions.js";
 
 export default class Holiday extends React.Component {
-
-    updateHolidayFavoriteState = (favoriteStatus, id) => {
-    fetch(`http://localhost:3000/api/v1/holidays/${id}.json`,
-    {
+  updateHolidayFavoriteState = (favoriteStatus, id) => {
+    fetch(`http://localhost:3000/api/v1/holidays/${id}.json`, {
       method: "PUT",
       headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json',
+        Accept: "application/json",
+        "Content-Type": "application/json"
       },
-      body: JSON.stringify({ holiday: {
-        favorite: (favoriteStatus === false ? 0 : 1)
-      }})
-    }
-  ).then(res => res.json())
-  .then(this.props.dispatch(changeFavorite(id, favoriteStatus)))
-  }
+      body: JSON.stringify({
+        holiday: {
+          favorite: favoriteStatus === false ? 0 : 1
+        }
+      })
+    })
+      .then(res => res.json())
+      .then(this.props.dispatch(changeFavorite(id, favoriteStatus)));
+  };
 
   render() {
-  return (
-    <>
-    <HolidayHeader date={this.props.date} name={this.props.name} />
-    <HolidayDescription description={this.props.description} />
-    <HolidayObservance observance={this.props.observance} />
-    <HolidayLocations locations={this.props.locations} />
-    <FavoriteButton favorite={this.props.favorite} id={this.props.id} updateHolidayFavoriteState={this.updateHolidayFavoriteState}/>
-    </>
-  )
+    return (
+      <>
+        <HolidayHeader date={this.props.date} name={this.props.name} />
+        <HolidayDescription description={this.props.description} />
+        <HolidayObservance observance={this.props.observance} />
+        <HolidayLocations locations={this.props.locations} />
+        <FavoriteButton
+          favorite={this.props.favorite}
+          id={this.props.id}
+          updateHolidayFavoriteState={this.updateHolidayFavoriteState}
+        />
+      </>
+    );
+  }
 }
-}
-
 
 const mapStateToProps = state => {
   return {
     holidays: state.holidays.holidays,
-    filteredHolidays: state.holidays.filteredHolidays,
+    filteredHolidays: state.holidays.filteredHolidays
   };
 };
+
 export const ConnectedHoliday = connect(mapStateToProps)(Holiday);
