4a03efd77e0d0a259815288e02787cc3d741bdfd
Eric Shiels <ericshiels2@gmail.com>
2019-04-11 17:00:25 -0400

    Prettify

--- TodaysHolidaysBackend/app/controllers/api/v1/holidays_controller.rb
+++ TodaysHolidaysBackend/app/controllers/api/v1/holidays_controller.rb
@@ -1,10 +1,10 @@
 class Api::V1::HolidaysController < ApplicationController
-  before_action :set_holiday, only: [:show, :update, :destroy]
+  before_action :set_holiday, only: %i[show update destroy]
 
   # GET /holidays
   # GET /holidays.json
   def index
-    #Holiday.select{ |holiday| holiday.locations.include?("QC") || holiday.locations.include?("NB")}
+    # Holiday.select{ |holiday| holiday.locations.include?("QC") || holiday.locations.include?("NB")}
     @holidays = Holiday.all
     render json: @holidays
   end
@@ -13,21 +13,22 @@ class Api::V1::HolidaysController < ApplicationController
   # PATCH/PUT /holidays/1.json
   def update
     if @holiday.update_attribute(:favorite, params[:holiday][:favorite])
-       #raise params.inspect
-       render json: @holiday, status: :ok
-     else
-       render json: @holiday.errors, status: :unprocessable_entity
+      # raise params.inspect
+      render json: @holiday, status: :ok
+    else
+      render json: @holiday.errors, status: :unprocessable_entity
      end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_holiday
-      @holiday = Holiday.find(params[:id])
-    end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def holiday_params
-      params.require(:holiday).permit(:name, :description, :date, :type, :locations, :states, :favorite)
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_holiday
+    @holiday = Holiday.find(params[:id])
+  end
+
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def holiday_params
+    params.require(:holiday).permit(:name, :description, :date, :type, :locations, :states, :favorite)
+  end
 end
