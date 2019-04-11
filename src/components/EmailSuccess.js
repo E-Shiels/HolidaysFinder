f4491522cb33f27752d185559db0be8d2495d06e
Eric Shiels <ericshiels2@gmail.com>
2019-04-11 17:32:00 -0400

    Prettify

--- src/components/EmailSuccess.js
+++ src/components/EmailSuccess.js
@@ -1,26 +1,17 @@
-import React from 'react'
-import { connect } from 'react-redux';
-//import { messageChange } from '../modules/actions/emailActions.js'
+import React from "react";
 
+import { connect } from "react-redux";
 
 export default class EmailSuccess extends React.Component {
-
-  // componentDidUpdate() {
-  //   this.props.dispatch(messageChange('Your email has been succesfully saved. Thanks!'))
-  //   setTimeout(this.props.dispatch(messageChange('')), 8000)
-  // }
-
   render() {
-    return(
-      <p>{this.props.message}</p>
-    )
+    return <p>{this.props.message}</p>;
   }
 }
 
-const mapStateToProps = (state) => {
+const mapStateToProps = state => {
   return {
     message: state.email.message
-  }
-}
+  };
+};
 
-export const ConnectedEmailSuccess = connect(mapStateToProps)(EmailSuccess)
+export const ConnectedEmailSuccess = connect(mapStateToProps)(EmailSuccess);
