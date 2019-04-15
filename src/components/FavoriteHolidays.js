be0529850c2c6c0ba32b5cc76f6fa6afe356ede3
Eric Shiels <ericshiels2@gmail.com>
2019-04-15 14:41:44 -0400

    Moves rendering of holidays from render() method to separate function

--- src/components/FavoriteHolidays.js
+++ src/components/FavoriteHolidays.js
@@ -13,6 +13,52 @@ export default class FavoriteHolidays extends React.Component {
     favoriteHolidays: []
   };
 
+  renderHolidays = () => {
+    if (!this.props.favoriteHolidays.length) {
+      return "You have no favorites.";
+    } else {
+      return this.props.favoriteHolidays.map(holiday => {
+        let locationsList = [""];
+        if (holiday.states.length === 1) {
+          locationsList.push({
+            name: holiday.states[0].name
+          });
+        } else if (holiday.states === "All") {
+          locationsList.push({
+            name: "Canada (All)"
+          });
+        } else {
+          holiday.states.split(",").forEach(state => {
+            locationsList.push({
+              name: state.trim()
+            });
+          });
+        }
+        let observanceList = [];
+        if (holiday.type.includes(",")) {
+          holiday.type.split(", ").forEach(type => {
+            observanceList.push(type.trim());
+          });
+        } else {
+          observanceList.push(holiday.type);
+        }
+
+        return (
+          <ConnectedHoliday
+            key={holiday.id}
+            id={holiday.id}
+            date={holiday.date}
+            name={holiday.name}
+            observance={observanceList}
+            description={holiday.description}
+            locations={locationsList}
+            favorite={holiday.favorite}
+          />
+        );
+      });
+    }
+  }
+
   toHolidayObjectsFromJSON = data => {
     let holidaysArray = [];
     data.forEach(holiday => {
@@ -49,49 +95,7 @@ export default class FavoriteHolidays extends React.Component {
     if (this.props.loading) {
       return "LOADING";
     } else {
-      if (!this.props.favoriteHolidays.length) {
-        return "You have no favorites.";
-      } else {
-        return this.props.favoriteHolidays.map(holiday => {
-          let locationsList = [""];
-          if (holiday.states.length === 1) {
-            locationsList.push({
-              name: holiday.states[0].name
-            });
-          } else if (holiday.states === "All") {
-            locationsList.push({
-              name: "Canada (All)"
-            });
-          } else {
-            holiday.states.split(",").forEach(state => {
-              locationsList.push({
-                name: state.trim()
-              });
-            });
-          }
-          let observanceList = [];
-          if (holiday.type.includes(",")) {
-            holiday.type.split(", ").forEach(type => {
-              observanceList.push(type.trim());
-            });
-          } else {
-            observanceList.push(holiday.type);
-          }
-
-          return (
-            <ConnectedHoliday
-              key={holiday.id}
-              id={holiday.id}
-              date={holiday.date}
-              name={holiday.name}
-              observance={observanceList}
-              description={holiday.description}
-              locations={locationsList}
-              favorite={holiday.favorite}
-            />
-          );
-        });
-      }
+      return this.renderHolidays();
     }
   }
 }
