4ebc672b2ead798a5fb244462b8aa55ef1ea6671
Eric Shiels <ericshiels2@gmail.com>
2019-04-15 17:38:11 -0400

    Code quality improvements + Prettify

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { ConnectedHolidaysContainer } from "./components/HolidaysContainer.js";
 import SiteFooter from "./components/SiteFooter.js";
 import { ConnectedEmailContainer } from "./components/EmailContainer.js";
 import Navbar from "./components/Navbar.js";
-import { ConnectedFavoriteHolidays } from './components/FavoriteHolidays.js';
+import { ConnectedFavoriteHolidays } from "./components/FavoriteHolidays.js";
 
 import NotFound from "./components/NotFound.js";
 
@@ -20,15 +20,18 @@ class App extends Component {
           <h1>Holidays</h1>
           <Switch>
             <Route path="/about" />
-            <Route path="/favorites" component={ConnectedFavoriteHolidays}/>
-            <Route exact path="/" render={props =>
-              <div>
-              <ConnectedHolidaysContainer />
-              <br/>
-              <br/>
-              <ConnectedEmailContainer />
-              </div>
-            }
+            <Route path="/favorites" component={ConnectedFavoriteHolidays} />
+            <Route
+              exact
+              path="/"
+              render={(props) => (
+                <div>
+                  <ConnectedHolidaysContainer />
+                  <br />
+                  <br />
+                  <ConnectedEmailContainer />
+                </div>
+              )}
             />
             <Route component={NotFound} />
           </Switch>
