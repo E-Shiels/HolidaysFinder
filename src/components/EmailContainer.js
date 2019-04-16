219295f722063d295d9bc4853a4eda20c32981ff
Eric Shiels <ericshiels2@gmail.com>
2019-04-16 15:44:41 -0400

    Update EmailContainer to reset email input to beginning after a time

--- src/components/EmailContainer.js
+++ src/components/EmailContainer.js
@@ -5,6 +5,11 @@ import { ConnectedEmailInput } from "./EmailInput.js";
 import { ConnectedEmailSuccess } from "./EmailSuccess.js";
 import { ConnectedEmailConfirm } from "./EmailConfirm.js";
 
+import {
+  emailResetStep,
+  emailChange
+} from "../modules/actions/emailActions.js";
+
 export class EmailContainer extends React.Component {
   render() {
     const { step } = this.props;
@@ -19,9 +24,19 @@ export class EmailContainer extends React.Component {
         return <ConnectedEmailSuccess />;
     }
   }
+
+  componentDidUpdate() {
+    if (this.props.step === 3) {
+      setTimeout(() => {
+        this.props.dispatch(emailChange(""));
+        this.props.dispatch(emailResetStep());
+
+      }, 15000);
+    }
+  }
 }
 
-const mapStateToProps = (state) => {
+const mapStateToProps = state => {
   return {
     step: state.email.step
   };
