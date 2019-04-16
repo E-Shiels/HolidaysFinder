5854293aa495b4fc586cef8645ccc2c852bff562
Eric Shiels <ericshiels2@gmail.com>
2019-04-16 13:40:50 -0400

    Rebuild database + clean data

--- TodaysHolidaysBackend/db/schema.rb
+++ TodaysHolidaysBackend/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_16_151115) do
+ActiveRecord::Schema.define(version: 2019_04_16_164359) do
 
   create_table "emails", force: :cascade do |t|
     t.string "email"
