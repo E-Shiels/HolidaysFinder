2b30aa19ad33419ee14354f30de4c15adec6585b
Eric Shiels <ericshiels2@gmail.com>
2019-04-16 15:49:06 -0400

    Prettify

--- src/modules/reducers/emailReducer.js
+++ src/modules/reducers/emailReducer.js
@@ -7,15 +7,15 @@ const initialState = {
 export default function emailReducer(state = initialState, action) {
   switch (action.type) {
     case "EMAIL_NEXT_STEP":
-      return {...state, step: action.step + 1};
+      return { ...state, step: action.step + 1 };
     case "EMAIL_PREV_STEP":
-      return {...state, step: action.step - 1};
+      return { ...state, step: action.step - 1 };
     case "EMAIL_RESET_STEP":
-      return {...state, step: 1}
+      return { ...state, step: 1 };
     case "EMAIL_CHANGE":
-      return {...state, email: action.email};
+      return { ...state, email: action.email };
     case "MESSAGE_CHANGE":
-      return {...state, message: action.message};
+      return { ...state, message: action.message };
     default:
       return state;
   }
