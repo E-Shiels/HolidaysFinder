2791f898d4ec69eb90d2f68bec7b17ab469774ab
Eric Shiels <ericshiels2@gmail.com>
2019-04-17 15:03:09 -0400

    Prettify and change icon button to named button

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,32 +1,60 @@
 import React from "react";
-import Home from "@material-ui/icons/Home"
+import HomeIcon from "@material-ui/icons/Home";
 
-import {
-  AppBar,
-  Toolbar,
-  Button,
-  IconButton
-} from "@material-ui/core"
+import { AppBar, Toolbar, Button } from "@material-ui/core";
+import { withStyles } from "@material-ui/core/styles";
 
-const Navbar = () => (
-  <div>
-    <AppBar position="static" color="primary">
-    <Toolbar>
-    <Button href="/">
-    <IconButton>
-    <Home />
-    </IconButton>
-      Home
-    </Button>
-      <Button href="/about">
-      About
-      </Button>
-      <Button href="/favorites">
-      View Favorites
-      </Button>
-      </Toolbar>
-    </AppBar>
-  </div>
-);
+const styles = theme => ({
+  button: {
+    margin: theme.spacing.unit
+  },
+  leftIcon: {
+    marginRight: theme.spacing.unit
+  }
+});
 
-export default Navbar;
+function Navbar(props) {
+  const { classes } = props;
+  return (
+    <div>
+      <AppBar position="static" color="primary">
+        <Toolbar>
+          <Button
+            href="/"
+            color="secondary"
+            variant="outlined"
+            className={classes.button}
+            size="medium"
+          >
+            <HomeIcon
+              color="secondary"
+              className={classes.leftIcon}
+              vertical-align="text-top"
+            />
+            Home
+          </Button>
+          <Button
+            href="/about"
+            color="secondary"
+            variant="outlined"
+            className={classes.button}
+            size="medium"
+          >
+            About
+          </Button>
+          <Button
+            href="/favorites"
+            color="secondary"
+            variant="outlined"
+            className={classes.button}
+            size="medium"
+          >
+            View Favorites
+          </Button>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
+}
+
+export default withStyles(styles)(Navbar);
