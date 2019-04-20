15056f52621f491d7bc248dec3b9b4449d5d5d08
Eric Shiels <ericshiels2@gmail.com>
2019-04-20 16:26:18 -0400

    Add MUI to About page

--- src/components/About.js
+++ src/components/About.js
@@ -1,7 +1,30 @@
 import React from "react";
+import { Typography } from "@material-ui/core";
+import { withStyles } from "@material-ui/core/styles";
 
-const About = () => {
-return <p>HolidaysFinder is a Flatiron school portfolio project. It allows you to view holidays in Canada and favorite and filter them. It was built using React, Create React App, and Redux, as well as assorted middleware and npm packages. The data is sent from a Rails API backend with archived data from <a href="https://calendarific.com/">https://calendarific.com/</a>.</p>
+const styles = theme => ({
+
+  centeredDiv: {
+    marginLeft: "auto",
+    marginRight: "auto",
+        width: "95%",
+  },
+  aboutBody: {
+    marginTop: theme.spacing.unit
+  },
+  header: {
+    marginTop: theme.spacing.unit * 2
+  }
+});
+
+const About = (props) => {
+  const { classes } = props;
+return (
+  <div className={classes.centeredDiv}>
+  <Typography className={classes.header} variant="h3">About</Typography>
+  <Typography className={classes.aboutBody} variant="body1">HolidaysFinder is a Flatiron school portfolio project. It allows you to view holidays in Canada and favorite and filter them. It was built using React, Create React App, and Redux, as well as assorted middleware and npm packages. The data is sent from a Rails API backend with archived data from <a href="https://calendarific.com/">https://calendarific.com/</a>.</Typography>
+  </div>
+)
 }
 
-export default About
+export default withStyles(styles)(About);
