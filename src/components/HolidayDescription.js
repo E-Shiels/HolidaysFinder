654362aa5392b891fca2f9f91145a2b4ea863461
Eric Shiels <ericshiels2@gmail.com>
2019-04-20 16:26:38 -0400

    Add MUI to Description

--- src/components/HolidayDescription.js
+++ src/components/HolidayDescription.js
@@ -1,5 +1,8 @@
 import React from "react";
+import { Typography } from "@material-ui/core";
 
-const HolidayDescription = (props) => <p>{props.description}</p>;
+const HolidayDescription = (props) => {
+  return <Typography>{props.description}</Typography>;
+}
 
 export default HolidayDescription;
