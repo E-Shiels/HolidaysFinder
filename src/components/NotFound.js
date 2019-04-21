9648cdf9525c4ba4ccdc904a874e34a153e1badb
Eric Shiels <ericshiels2@gmail.com>
2019-04-21 16:12:31 -0400

    Prettify

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Typography } from "@material-ui/core";
 import { withStyles } from "@material-ui/core/styles";
-import ErrorIcon from "@material-ui/icons/Error"
+import ErrorIcon from "@material-ui/icons/Error";
 
 const styles = theme => ({
   centeredDiv: {
@@ -16,23 +16,26 @@ const styles = theme => ({
     marginTop: theme.spacing.unit * 2
   },
   icon: {
-        position: "relative",
-        width: "1.7em",
-        height: "1.7em"
-    }
+    position: "relative",
+    width: "1.7em",
+    height: "1.7em"
+  }
 });
 
-const notFound = (props) => {
+const notFound = props => {
   const { classes } = props;
   return (
-  <div className={classes.centeredDiv}>
-    <Typography className={classes.header} variant="h2">
-    <ErrorIcon color="inherit" className={classes.icon}></ErrorIcon>
-    Error - 404
-    </Typography>
-    <Typography className={classes.body} variant="body1">The page you were looking for could not be found. Try pressing one of the buttons in the navigation bar.</Typography>
-  </div>
-)
+    <div className={classes.centeredDiv}>
+      <Typography className={classes.header} variant="h2">
+        <ErrorIcon color="inherit" className={classes.icon} />
+        Error - 404
+      </Typography>
+      <Typography className={classes.body} variant="body1">
+        The page you were looking for could not be found. Try pressing one of
+        the buttons in the navigation bar.
+      </Typography>
+    </div>
+  );
 };
 
 export default withStyles(styles)(notFound);
