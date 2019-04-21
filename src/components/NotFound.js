405271f55f089f901f5c7cf43a60e6d2dbc3897d
Eric Shiels <ericshiels2@gmail.com>
2019-04-20 21:08:58 -0400

    Add MUI to not found

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -1,10 +1,38 @@
 import React from "react";
+import { Typography } from "@material-ui/core";
+import { withStyles } from "@material-ui/core/styles";
+import ErrorIcon from "@material-ui/icons/Error"
 
-const notFound = () => (
-  <div>
-    <h1>Error - 404</h1>
-    <h3>The page you were looking for could not be found.</h3>
+const styles = theme => ({
+  centeredDiv: {
+    marginLeft: "auto",
+    marginRight: "auto",
+    width: "95%"
+  },
+  body: {
+    marginTop: theme.spacing.unit * 2
+  },
+  header: {
+    marginTop: theme.spacing.unit * 2
+  },
+  icon: {
+        position: "relative",
+        width: "1.7em",
+        height: "1.7em"
+    }
+});
+
+const notFound = (props) => {
+  const { classes } = props;
+  return (
+  <div className={classes.centeredDiv}>
+    <Typography className={classes.header} variant="h2">
+    <ErrorIcon color="inherit" className={classes.icon}></ErrorIcon>
+    Error - 404
+    </Typography>
+    <Typography className={classes.body} variant="body1">The page you were looking for could not be found. Try pressing one of the buttons in the navigation bar.</Typography>
   </div>
-);
+)
+};
 
-export default notFound;
+export default withStyles(styles)(notFound);
