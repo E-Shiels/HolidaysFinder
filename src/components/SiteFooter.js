d0cc9a1c31b7747d10905602e5c691a04ae060ef
Eric Shiels <ericshiels2@gmail.com>
2019-04-21 19:01:31 -0400

    Rebuilding footer CSS

--- src/components/SiteFooter.js
+++ src/components/SiteFooter.js
@@ -2,21 +2,21 @@ import React from "react";
 
 var style = {
   backgroundColor: "#F8F8F8",
-  borderTop: "1px solid #E7E7E7",
-  textAlign: "center",
-  padding: "0px 10px 15px 10px",
-  position: "fixed",
-  left: "0",
-  bottom: "0",
-  height: "30px",
-  width: "100%"
+    borderTop: "1px solid #E7E7E7",
+    textAlign: "center",
+    padding: "0px",
+    position: "fixed",
+    left: "0",
+    bottom: "0",
+    height: "50px",
+    width: "100%",
 };
 
 var phantom = {
-  display: "block",
-  padding: "10px",
-  height: "30px",
-  width: "100%"
+  display: 'block',
+   padding: '20px',
+   height: '50px',
+   width: '100%',
 };
 
 export default function SiteFooter() {
