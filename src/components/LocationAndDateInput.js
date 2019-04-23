dcd40929e8e410fb74e74faf93335de3c83ae87a
Eric Shiels <ericshiels2@gmail.com>
2019-04-23 14:58:53 -0400

    Remove unused MUI element

--- src/components/LocationAndDateInput.js
+++ src/components/LocationAndDateInput.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Typography, Button, Paper, Divider } from "@material-ui/core";
+import { Typography, Button, Paper } from "@material-ui/core";
 
 import Select from "react-select";
 import "react-dates/initialize";
