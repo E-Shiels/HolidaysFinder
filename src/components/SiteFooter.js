4e24d54f385317c87d25cd64462df836553c9a82
Eric Shiels <ericshiels2@gmail.com>
2019-04-22 20:11:52 -0400

    Update SiteFooter to use theme colors

--- src/components/SiteFooter.js
+++ src/components/SiteFooter.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 var style = {
-  backgroundColor: "#F8F8F8",
+  backgroundColor: "#616161",
     borderTop: "1px solid #E7E7E7",
     textAlign: "center",
     padding: "0px",
@@ -13,21 +13,25 @@ var style = {
 };
 
 var phantom = {
-  display: 'block',
-   padding: '20px',
-   height: '50px',
-   width: '100%',
+  display: "block",
+   padding: "20px",
+   height: "50px",
+   width: "100%",
 };
+
+var text = {
+  color: "rgb(250, 250, 250)"
+}
 
 export default function SiteFooter() {
   return (
     <div>
       <div style={phantom} />
       <div style={style}>
-        <p>
+        <p style={text}>
           Website Design and Development by{" "}
-          <a href="https://www.resays.com/">Eric Shiels.</a> Holiday data
-          courtesy of <a href="https://calendarific.com/">Calendarific</a>
+          <a href="https://www.resays.com/" style={text}>Eric Shiels.</a> Holiday data
+          courtesy of <a href="https://calendarific.com/" style={text}>Calendarific.</a>
         </p>
       </div>
     </div>
