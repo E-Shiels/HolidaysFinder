5515ce9197131cd44cd5d13c9e92455f306c474d
Eric Shiels <ericshiels2@gmail.com>
2019-04-23 19:00:42 -0400

    Remove logger and old comments

--- src/modules/store/store.js
+++ src/modules/store/store.js
@@ -1,18 +1,15 @@
 import { createStore, applyMiddleware, combineReducers, compose } from "redux";
-import logger from "redux-logger";
 import thunk from "redux-thunk";
 
 import emailReducer from "../reducers/emailReducer.js";
 import holidayReducer from "../reducers/holidayReducer.js";
-//import other reducers
 
 const rootReducer = combineReducers({
   email: emailReducer,
   holidays: holidayReducer
-  //import other reducers
 });
 
-const middleware = [thunk, logger];
+const middleware = [thunk];
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
