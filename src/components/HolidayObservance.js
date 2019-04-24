04c2687fd25cc1ba0fd003c48789361fae9ff259
Eric Shiels <ericshiels2@gmail.com>
2019-04-24 14:30:47 -0400

    Prettify

--- src/components/HolidayObservance.js
+++ src/components/HolidayObservance.js
@@ -1,9 +1,11 @@
 import React from "react";
-import { Typography } from "@material-ui/core"
+import { Typography } from "@material-ui/core";
 
 const HolidayObservance = (props) => {
   if (props.observance.length > 1) {
-    return <Typography variant="body1">{props.observance.join(" - ")}</Typography>;
+    return (
+      <Typography variant="body1">{props.observance.join(" - ")}</Typography>
+    );
   } else {
     return <Typography variant="body1">{props.observance}</Typography>;
   }
--- src/components/SiteFooter.js
+++ src/components/SiteFooter.js
@@ -2,26 +2,26 @@ import React from "react";
 
 var style = {
   backgroundColor: "#616161",
-    borderTop: "1px solid #E7E7E7",
-    textAlign: "center",
-    padding: "0px",
-    position: "fixed",
-    left: "0",
-    bottom: "0",
-    height: "50px",
-    width: "100%",
+  borderTop: "1px solid #E7E7E7",
+  textAlign: "center",
+  padding: "0px",
+  position: "fixed",
+  left: "0",
+  bottom: "0",
+  height: "50px",
+  width: "100%"
 };
 
 var phantom = {
   display: "block",
-   padding: "20px",
-   height: "50px",
-   width: "100%",
+  padding: "20px",
+  height: "50px",
+  width: "100%"
 };
 
 var text = {
   color: "rgb(250, 250, 250)"
-}
+};
 
 export default function SiteFooter() {
   return (
@@ -30,8 +30,13 @@ export default function SiteFooter() {
       <div style={style}>
         <p style={text}>
           Website Design and Development by{" "}
-          <a href="https://www.resays.com/" style={text}>Eric Shiels.</a> Holiday data
-          courtesy of <a href="https://calendarific.com/" style={text}>Calendarific.</a>
+          <a href="https://www.resays.com/" style={text}>
+            Eric Shiels.
+          </a>{" "}
+          Holiday data courtesy of{" "}
+          <a href="https://calendarific.com/" style={text}>
+            Calendarific.
+          </a>
         </p>
       </div>
     </div>
