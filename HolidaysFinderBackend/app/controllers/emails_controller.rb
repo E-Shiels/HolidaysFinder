da4d1baf4206f101ed7a196caaa1b528f9f7423f
Eric Shiels <ericshiels2@gmail.com>
2019-04-24 14:38:30 -0400

    Prettify

--- HolidaysFinderBackend/app/controllers/emails_controller.rb
+++ HolidaysFinderBackend/app/controllers/emails_controller.rb
@@ -1,5 +1,4 @@
 class EmailsController < ApplicationController
-
   # POST /emails
   # POST /emails.json
   def create
@@ -12,8 +11,8 @@ class EmailsController < ApplicationController
     end
   end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def email_params
-      params.require(:email).permit(:email)
-    end
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def email_params
+    params.require(:email).permit(:email)
+  end
 end
